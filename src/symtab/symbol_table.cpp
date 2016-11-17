05a583af1534b994b453143933f2126f1a413d28
Martin Yrjölä <martin.yrjola@gmail.com>
2016-11-17 15:43:34 +0100

    Formatting

--- src/symtab/symbol_table.cpp
+++ src/symtab/symbol_table.cpp
@@ -6,58 +6,58 @@ using std::string;
 
 SymbolTable::SymbolTable(void)
 {
-    /* TASK: Add whatever code you find necessary */
+  /* TASK: Add whatever code you find necessary */
 }
 
 SymbolTable::~SymbolTable(void) {
-    /* TASK: Add whatever code you find necessary */
+  /* TASK: Add whatever code you find necessary */
 }
 
 SymbolTable::Record* SymbolTable::lookUp(const string& name) const {
-    /* TASK: Implement this method */
+  /* TASK: Implement this method */
 }
 
 bool SymbolTable::insert(const string& name, int line, int column) {
-    /* TASK: Implement this method */
+  /* TASK: Implement this method */
 }
 
 void SymbolTable::clear(void) {
-    /* TASK: Implement this method */
+  /* TASK: Implement this method */
 }
 
 list<SymbolTable::Record*> SymbolTable::getRecords(void) const {
-    /* TASK: Implement this method */
+  /* TASK: Implement this method */
 }
 
 SymbolTable::Record::Record(
-    const string& name,
-    int line,
-    int column,
-    int memory_index)
-        : _name(name),
-          _line(line),
-          _column(column),
-          _memory_index(memory_index)
+                            const string& name,
+                            int line,
+                            int column,
+                            int memory_index)
+  : _name(name),
+    _line(line),
+    _column(column),
+    _memory_index(memory_index)
 {}
 
 SymbolTable::Record::~Record(void) {}
 
 string SymbolTable::Record::getName(void) const {
-    return _name;
+  return _name;
 }
 
 int SymbolTable::Record::getLine(void) const {
-    return _line;
+  return _line;
 }
 
 int SymbolTable::Record::getColumn(void) const {
-    return _column;
+  return _column;
 }
 
 int SymbolTable::Record::getMemoryIndex(void) const {
-    return _memory_index;
+  return _memory_index;
 }
 
 void SymbolTable::Record::setMemoryIndex(int index) {
-    _memory_index = index;
+  _memory_index = index;
 }
