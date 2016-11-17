af9ec7d2bfb8b6e8d0529001f880b3b97ca1f8e5
Martin Yrjölä <martin.yrjola@gmail.com>
2016-11-17 15:42:43 +0100

    Formatting

--- src/symtab/symbol_table.hpp
+++ src/symtab/symbol_table.hpp
@@ -43,157 +43,157 @@
  * as the AST is traversed.
  */
 class SymbolTable {
+public:
+  class Record;
+
+public:
+  /**
+   * Creates a symbol table.
+   */
+  SymbolTable(void);
+
+  /**
+   * Destroys this symbol table, and all records that it contains.
+   */
+  ~SymbolTable(void);
+
+  /**
+   * Gets the record with a given identifier name. If no such record is found,
+   * \c NULL is returned.
+   *
+   * @param name
+   *        Identifier name.
+   * @returns Record with matching name.
+   */
+  Record* lookUp(const std::string& name) const;
+
+  /**
+   * Adds an identifier with a given name to this symbol table. If an
+   * identifier with an identical name has already been inserted, no change is
+   * made and false is returned.
+   *
+   * @param name
+   *        Identifier name.
+   * @param line
+   *        Line number where the identifer was declared.
+   * @param column
+   *        Column number where the identifer was declared.
+   * @returns \c true if the identifier was inserted.
+   */
+  bool insert(const std::string& name, int line, int column);
+
+  /**
+   * Gets a list of all records in the symbol table. The returned list need
+   * not be any particular order (this method is used for inspection purposes
+   * only).
+   *
+   * @returns All records.
+   */
+  std::list<Record*> getRecords(void) const;
+
+  /**
+   * Clears the entire symbol table and destroys all records.
+   */
+  void clear(void);
+
+private:
+  /* TASK: Add whatever private members and methods you find necessary */
+
+public:
+  /**
+   * \brief Class for representing entries in the symbol table.
+   *
+   * The Record class defines the name of the identifier, the line number at
+   * which it declard, and a new memory index. The latter will be used to
+   * specify where to store the value of the identifier at run time.
+   */
+  class Record {
   public:
-    class Record;
-
-  public:
     /**
-     * Creates a symbol table.
-     */
-    SymbolTable(void);
-
-    /**
-     * Destroys this symbol table, and all records that it contains.
-     */
-    ~SymbolTable(void);
-
-    /**
-     * Gets the record with a given identifier name. If no such record is found,
-     * \c NULL is returned.
+     * Creates a record for an identifier.
      *
      * @param name
-     *        Identifier name.
-     * @returns Record with matching name.
-     */
-    Record* lookUp(const std::string& name) const;
-
-    /**
-     * Adds an identifier with a given name to this symbol table. If an
-     * identifier with an identical name has already been inserted, no change is
-     * made and false is returned.
-     *
-     * @param name
-     *        Identifier name.
+     *        Name of the identifier.
      * @param line
-     *        Line number where the identifer was declared.
+     *        Line number at which the identifier was declared.
      * @param column
-     *        Column number where the identifer was declared.
-     * @returns \c true if the identifier was inserted.
+     *        Column number at which the identifier was declared.
+     * @param memory_index
+     *        Memory index at where the value of the identifier will
+     *        be stored.
      */
-    bool insert(const std::string& name, int line, int column);
+    Record(
+           const std::string& name,
+           int line,
+           int column,
+           int memory_index);
 
     /**
-     * Gets a list of all records in the symbol table. The returned list need
-     * not be any particular order (this method is used for inspection purposes
-     * only).
+     * Destroys this record.
+     */
+    ~Record(void);
+
+    /**
+     * Gets the identifier name of this record.
      *
-     * @returns All records.
+     * @returns Identifier name.
      */
-    std::list<Record*> getRecords(void) const;
+    std::string getName(void) const;
 
     /**
-     * Clears the entire symbol table and destroys all records.
-     */
-    void clear(void);
-
-  private:
-    /* TASK: Add whatever private members and methods you find necessary */
-
-  public:
-    /**
-     * \brief Class for representing entries in the symbol table.
+     * Gets the line number at which the identifier of this record was
+     * declared
      *
-     * The Record class defines the name of the identifier, the line number at
-     * which it declard, and a new memory index. The latter will be used to
-     * specify where to store the value of the identifier at run time.
+     * @returns Line number.
      */
-    class Record {
-      public:
-        /**
-         * Creates a record for an identifier.
-         *
-         * @param name
-         *        Name of the identifier.
-         * @param line
-         *        Line number at which the identifier was declared.
-         * @param column
-         *        Column number at which the identifier was declared.
-         * @param memory_index
-         *        Memory index at where the value of the identifier will
-         *        be stored.
-         */
-        Record(
-            const std::string& name,
-            int line,
-            int column,
-            int memory_index);
+    int getLine(void) const;
 
-        /**
-         * Destroys this record.
-         */
-        ~Record(void);
+    /**
+     * Gets the column number at which the identifier of this record was
+     * declared
+     *
+     * @returns Column number.
+     */
+    int getColumn(void) const;
 
-        /**
-         * Gets the identifier name of this record.
-         * 
-         * @returns Identifier name.
-         */
-        std::string getName(void) const;
+    /**
+     * Gets the memory index of this record.
+     *
+     * @returns Memory index.
+     */
+    int getMemoryIndex(void) const;
 
-        /**
-         * Gets the line number at which the identifier of this record was
-         * declared
-         * 
-         * @returns Line number.
-         */
-        int getLine(void) const;
-
-        /**
-         * Gets the column number at which the identifier of this record was
-         * declared
-         * 
-         * @returns Column number.
-         */
-        int getColumn(void) const;
-
-        /**
-         * Gets the memory index of this record.
-         *
-         * @returns Memory index.
-         */
-        int getMemoryIndex(void) const;
-
-        /**
-         * Sets the memory index to use for this record. Through liveness
-         * analysis, two or more records can be set to share the same memory
-         * location, thus reducing the memory footprint.
-         *
-         * @param index
-         *        Memory index.
-         */
-        void setMemoryIndex(int index);
+    /**
+     * Sets the memory index to use for this record. Through liveness
+     * analysis, two or more records can be set to share the same memory
+     * location, thus reducing the memory footprint.
+     *
+     * @param index
+     *        Memory index.
+     */
+    void setMemoryIndex(int index);
         
-      private:
-        /**
-         * Name of the variable.
-         */
-        std::string _name;
+  private:
+    /**
+     * Name of the variable.
+     */
+    std::string _name;
 
-        /**
-         * Line number at which the variable was declared.
-         */
-        int _line;
+    /**
+     * Line number at which the variable was declared.
+     */
+    int _line;
 
-        /**
-         * Column number at which the variable was declared.
-         */
-        int _column;
+    /**
+     * Column number at which the variable was declared.
+     */
+    int _column;
 
-        /**
-         * Memory index assigned to the variable.
-         */
-        int _memory_index;
-    };
+    /**
+     * Memory index assigned to the variable.
+     */
+    int _memory_index;
+  };
 };
 
 #endif
